ca9a0d7d8a093def93ece50b5933898aabff1d53
hamidraza1 <hrazaa306@gmail.com>
2020-08-10 20:45:32 +0200

    Updates

---
+++ precache-manifest.dc97071310fa34350cf48e10f0259cb8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc52b6a2bf65b3c8f22ab8494b639ed7",
+    "url": "/Awards/index.html"
+  },
+  {
+    "revision": "a0a7997ecd241c49b7ae",
+    "url": "/Awards/static/css/main.f828c15c.chunk.css"
+  },
+  {
+    "revision": "27329362e99fa983b5b7",
+    "url": "/Awards/static/js/2.7b0a92b4.chunk.js"
+  },
+  {
+    "revision": "f5327a3e8204f21ff1b2e5af38bc59ba",
+    "url": "/Awards/static/js/2.7b0a92b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0a7997ecd241c49b7ae",
+    "url": "/Awards/static/js/main.da60d935.chunk.js"
+  },
+  {
+    "revision": "63b0bf602f00437641da",
+    "url": "/Awards/static/js/runtime-main.7cbbbfe4.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/Awards/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/Awards/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "ea0dc5634b4fc384f2cc571b1062285e",
+    "url": "/Awards/static/media/up-arrow-circle.ea0dc563.svg"
+  }
+]);
