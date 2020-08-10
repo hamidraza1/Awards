382797d0c8fa9d5792883b6f38830b1585337046
hamidraza1 <hrazaa306@gmail.com>
2020-08-10 19:12:10 +0200

    Updates

---
+++ precache-manifest.6ad7d282265d08a685c98feda460e6b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2be6dc25f0da3b4ce8726e20093e1d",
+    "url": "/hamidraza1/Awards.git/index.html"
+  },
+  {
+    "revision": "e6f8977d99a82b5d5f6c",
+    "url": "/hamidraza1/Awards.git/static/css/main.f828c15c.chunk.css"
+  },
+  {
+    "revision": "7786b3354b0bed681f7e",
+    "url": "/hamidraza1/Awards.git/static/js/2.d341043f.chunk.js"
+  },
+  {
+    "revision": "f5327a3e8204f21ff1b2e5af38bc59ba",
+    "url": "/hamidraza1/Awards.git/static/js/2.d341043f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f8977d99a82b5d5f6c",
+    "url": "/hamidraza1/Awards.git/static/js/main.873e097a.chunk.js"
+  },
+  {
+    "revision": "260c38271fb722e4b0c3",
+    "url": "/hamidraza1/Awards.git/static/js/runtime-main.d7165c19.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/hamidraza1/Awards.git/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/hamidraza1/Awards.git/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/hamidraza1/Awards.git/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/hamidraza1/Awards.git/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "ea0dc5634b4fc384f2cc571b1062285e",
+    "url": "/hamidraza1/Awards.git/static/media/up-arrow-circle.ea0dc563.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/hamidraza1/Awards.git/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
